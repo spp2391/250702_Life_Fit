3fefc20219274cd8c07fbcc8fb440c7e73a1dc69
DESKTOP-NFVDMDM\it <heesu04092@naver.com>
2025-07-02 14:34:30 +0900

    Category 테이블 생성

---
+++ Life_Fit/src/main/java/org/zerock/life_fit/board/domain/BoardCate.java
@@ -0,0 +1,23 @@
+package org.zerock.life_fit.board.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Table(name = "BOARD")
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class BoardCate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cateno", updatable = false)
+    private int cateno;
+    @Column(name = "filename", nullable = false)
+    private String filename;
+    @Column(name = "cate", nullable = false)
+    private boolean cate;
+}
