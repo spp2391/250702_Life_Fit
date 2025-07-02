0bf9de894a69e6e2d20ca255575c4abb2606d1a8
DESKTOP-9RMARTO\it <spp2391@naver.com>
2025-07-02 12:06:29 +0900

    2025-07-02 최초 프로젝트생성

---
+++ Life_Fit/src/main/java/org/zerock/life_fit/LifeFitApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.life_fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeFitApplication.class, args);
+    }
+
+}
---
+++ Life_Fit/src/test/java/org/zerock/life_fit/LifeFitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.life_fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeFitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
