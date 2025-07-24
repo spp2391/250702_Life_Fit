a6777468a73360adcf469e4966d1d2d6c864d6b4
DESKTOP-TN9NCGJ\it <wowls5069@naver.com>
2025-07-24 17:39:23 +0900

    7.24 css수정

--- Life_Fit/src/main/java/org/zerock/life_fit/mainscreen/station/repository/StationRepository.java
+++
@@ -1,7 +0,0 @@
-package org.zerock.life_fit.mainscreen.station.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.zerock.life_fit.mainscreen.station.domain.BusanStation;
-
-public interface StationRepository extends JpaRepository<BusanStation, Long> {
-}
